707cf0865ac09cf92cb5c3b529a624c04aa94a47
bayogluteoman <bayogluteoman@gmail.com>
2021-01-29 23:00:55 +0300

    update corresponding components with new redux flow for directory/shop

--- shopping project/src/components/collections-overview/collections-overview.component.js
+++ shopping project/src/components/collections-overview/collections-overview.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector} from 'reselect';
 import  CollectionPreview  from '../collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) =>(
@@ -15,7 +15,7 @@ const CollectionsOverview = ({ collections }) =>(
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect (mapStateToProps)(CollectionsOverview); 
--- shopping project/src/pages/collection/collection.component.js
+++ shopping project/src/pages/collection/collection.component.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-//import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-    console.log(collection);
+const {title, items}=collection;
     return(  
     <div className='collection-page'> 
-    <h2>COLLECTION PAGE</h2>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            {
+                items.map(item => <CollectionItem key={item.id} item={item}/>)
+            }
+        </div>
     </div>
  );
 };
